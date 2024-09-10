c9d268618dbe812bbaaaa9b89426a7d5afafce1e
Jerazo1277 <jovanna.erazo.daza@gmail.com>
2024-09-09 20:38:45 -0500

    Abordamos hasta la sesion 2 de fastappi

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI 
+from fastapi.responses import HTMLResponse
 app = FastAPI()
 app.title = "Mi aplicación con FastAPI"
 app.version = "0.0.1"
+
+movie_list = [
+    {
+        "id":1,
+        "title": "Spiderman",
+        "overview": "Un superheroe",
+        "year": 2021,
+        "rating": 5.0
+    },
+     {
+        "id":2,
+        "title": "Intensamente 2",
+        "overview": "Emociones intensas 2",
+        "year": 2021,
+        "rating": 10.0
+    },
+      {
+        "id":3,
+        "title": "Mis huellas a casa",
+        "overview": "Mascota",
+        "year": 2020,
+        "rating": 8.0
+    }
+    
+]
 @app.get('/', tags=['Home'])
 def message():
-    return 'Hello World'
+    return HTMLResponse('<h1>¡Hola mundo!</h1>')
+@app.get('/movies', tags=['Movies'])
+def movies():
+    return movie_list
